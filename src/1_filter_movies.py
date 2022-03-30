c6c442094e68e9270cfe2a7d590fe10d0eee185c
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-03-29 19:03:46 -0600

    Improved task 1

--- src/1_filter_movies.py
+++ src/1_filter_movies.py
@@ -1,12 +1,15 @@
 from pyspark import SparkContext
+from os.path import join
 
+BASE = 'file:///s/chopin/a/grad/gmaurina/workspace/cs535/team_project'
 INPUT = 'data/title.basics.tsv'
 OUTPUT = 'data/movies.tsv'
 
 def main():
     sc = SparkContext('local', '1_filter_movies')
     sc.setLogLevel('ERROR')
-    sc.textFile(INPUT)
+    movies = sc.textFile(join(BASE, INPUT))
+    movies.take(10).foreach(println)
 
 if __name__ == '__main__':
     main()

c8477130625561cb2b9ada9f99dfd7e49a16bf0f
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-03-29 20:36:53 -0600

    Fixed error

--- src/1_filter_movies.py
+++ src/1_filter_movies.py
@@ -7,7 +7,7 @@ def main():
     sc = SparkContext('local', '1_filter_movies')
     sc.setLogLevel('ERROR')
     movies = sc.textFile(INPUT)
-    movies = movies.map(lambda v: v.split('\t')).filter(lambda v: v[1]=='movie' and v[4]=='0').map(lambda v: '\t'.join((v[0],v[2],v[3],v[5]))).collect()
+    movies = movies.map(lambda v: v.split('\t')).filter(lambda v: v[1]=='movie' and v[4]=='0' and v[5]!='\N').map(lambda v: '\t'.join((v[0],v[2],v[3],v[5]))).collect()
     with open(OUTPUT, 'w', encoding='utf-8') as out:
         out.write('tconst\tprimaryTitle\toriginalTitle\tstartYear\n')
         out.write('\n'.join(movies))

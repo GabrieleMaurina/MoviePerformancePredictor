c33a281ecc5ba63f6d7647b69a5c81c9390bf111
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-03-29 20:32:23 -0600

    Fixed error

--- src/1_filter_movies.py
+++ src/1_filter_movies.py
@@ -8,7 +8,7 @@ def main():
     sc.setLogLevel('ERROR')
     movies = sc.textFile(INPUT)
     movies = movies.map(lambda v: v.split('\t')).filter(lambda v: v[1]=='movie' and v[4]=='0').map(lambda v: '\t'.join((v[0],v[2],v[3],v[5]))).collect()
-    with open('data/movies.tsv', 'w', encoding='utf-8') as out:
+    with open(OUTPUT, 'w', encoding='utf-8') as out:
         out.write('tconst\tprimaryTitle\toriginalTitle\tstartYear\n')
         out.write('\n'.join(movies))
 

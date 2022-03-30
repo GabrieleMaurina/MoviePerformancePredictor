1c9d5ebce4713142e6faa07a1c882e1796c89857
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-03-29 20:25:12 -0600

    Filtered movies

--- src/1_filter_movies.py
+++ src/1_filter_movies.py
@@ -1,15 +1,16 @@
 from pyspark import SparkContext
-from os.path import join
 
-BASE = 'file:///s/chopin/a/grad/gmaurina/workspace/cs535/team_project'
 INPUT = 'data/title.basics.tsv'
 OUTPUT = 'data/movies.tsv'
 
 def main():
     sc = SparkContext('local', '1_filter_movies')
     sc.setLogLevel('ERROR')
-    movies = sc.textFile(join(BASE, INPUT))
-    movies.take(10).foreach(println)
+    movies = sc.textFile(INPUT)
+    movies = movies.map(lambda v: v.split('\t')).filter(lambda v: v[1]=='movie' and v[4]=='0').map(lambda v: '\t'.join((v[0],v[2],v[3],v[5]))).collect()
+    with open('data/movies.tsv', 'w', encoding='utf-8') as out:
+        out.write('tconst\tprimaryTitle\toriginalTitle\tstartYear\n')
+        out.write('\n'.join(movies))
 
 if __name__ == '__main__':
     main()

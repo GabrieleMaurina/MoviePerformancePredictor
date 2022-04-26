533671f8cb30b829c29d7fcdb99de0c4063dbbdd
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-04-26 11:48:27 -0600

    Added adjustment for inflation

--- src/3_join_data.py
+++ src/3_join_data.py
@@ -2,8 +2,7 @@ from pyspark import SparkContext
 from pyspark.sql.types import IntegerType, FloatType
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import split, when, array_contains
-from pyspark.sql.functions import *
-import pyspark.sql.functions as func    
+from cpi import inflate
 
 
 OUTPUT = 'data/data.tsv'
@@ -64,6 +63,11 @@ def main():
     data = data.join(title_ratings, 'tconst')
     data = data.join(title_akas, 'tconst')
 
+    #adjust for inflation
+    adjust = udf(lambda value, year: int(inflate(value, year)))
+    data = data.withColumn('budget', adjust(data.budget, data.startYear))
+    data = data.withColumn('box_offce', adjust(data.box_offce, data.startYear))
+
     #encoding genres
     GENRES = ('Action', 'Adult', 'Adventure', 'Animation', 'Biography',
         'Comedy','Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History',
@@ -72,13 +76,13 @@ def main():
     for genre in GENRES:
         data = data.withColumn(genre, when(array_contains('genres', genre), 1).otherwise(0)) #add a column for each genre
     data = data.drop('genres') #remove genres column
-    
+
     #addition for principals (Azlan 4/25/2022)
     title_crew = title_crew.withColumn("directors", concat_ws(",",col("directors")))
     title_crew = title_crew.withColumn("writers", concat_ws(",",col("writers")))
 
     title_crew = title_crew.withColumn('director_1', split(title_crew['directors'], ',').getItem(0)) \
-           .withColumn('director_2', split(title_crew['directors'], ',').getItem(1)) 
+           .withColumn('director_2', split(title_crew['directors'], ',').getItem(1))
 
     title_crew = title_crew.withColumn('writer_1', split(title_crew['writers'], ',').getItem(0)) \
            .withColumn('writer_2', split(title_crew['writers'], ',').getItem(1)) \
@@ -89,11 +93,11 @@ def main():
            .withColumn('writer_3', concat(col('writer_3'), lit(',writer,'), col('tconst')))
 
     title_crew = title_crew.withColumn('director_1', concat(col('director_1'), lit(',director,'), col('tconst'))) \
-         .withColumn('director_2', concat(col('director_2'), lit(',director,'), col('tconst'))) 
-    
+         .withColumn('director_2', concat(col('director_2'), lit(',director,'), col('tconst')))
+
     crew_final = title_crew.select(explode(array(
             col("director_1"),
-            col("director_2"), 
+            col("director_2"),
             col("writer_1"),
             col("writer_2"),
             col("writer_3")))).distinct()
@@ -101,7 +105,7 @@ def main():
     crew_final = crew_final.withColumn('tconst', split(crew_final['col'], ',').getItem(2)) \
           .withColumn('nconst', split(crew_final['col'], ',').getItem(0)) \
           .withColumn('category', split(crew_final['col'], ',').getItem(1)).select('tconst', 'nconst', 'category')
-    
+
     df = title_principals.join(title_ratings, 'tconst')
     df2 = crew_final.join(title_ratings, 'tconst')
     principals1 = df2.groupby('nconst').agg(countDistinct('tconst')).alias('titles_ct')
@@ -119,7 +123,7 @@ def main():
     principals = principals_df1.union(principals_df2)
     principals.show()
 
-    
+
     # Encoding cast and crew (directors, writers, actors)
     #for i in new_6.select("category").distinct().collect():
     #    new_6 = new_6.withColumn(i, when(new_6['category'].contains(i), nconst).otherwise(0))

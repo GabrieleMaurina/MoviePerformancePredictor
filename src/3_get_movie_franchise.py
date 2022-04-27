28aa0709f694c51a325f6f13ed9b5a2f4f978464
GabrieleMaurina <gabrielemaurina95@gmail.com>
2022-04-27 15:53:20 -0600

    Improved franchise

--- src/3_get_movie_franchise.py
+++ src/3_get_movie_franchise.py
@@ -15,35 +15,38 @@ def franchise(title1, title2):
     match_size = matcher.find_longest_match(0,len(title1),0,len(title2)).size
     return ratio > 0.9 or (ratio > 0.7 and match_size > 7) or match_size > 12
 
-def dfs(movies, movie):
+def dfs(movies, movie, f):
     movie[1] = False
+    tot = 1
     for neighbor in movies[id][2]:
         neighbor = movies[neighbor]
-        if neighbor[1]:
-            dfs(movies, neighbor)
+        if neighbor[2]:
+            tot += dfs(movies, neighbor, f)
+    return tot
 
 def main():
     with open(INPUT, 'r') as movies:
         movies = tuple(movie.split('\t')[:2] for movie in movies.read().split('\n') if movie and not movie.startswith('tconst'))
-    movies = {movie[0]: [movie[1], preprocess_title(movie[1]), True, []] for movie in movies}
+    movies = {movie[0]: [movie[1], preprocess_title(movie[1]), True, [], 0] for movie in movies}
     edges = 0
     tot = len(movies)
-    print(tot)
     movies_tuple = tuple(movies.items())
     for i in range(tot):
-        id1, (title1, pptitle1, _, neighbors1) = movies_tuple[i]
-        print(i/tot)
+        id1, (title1, pptitle1, _, neighbors1, _) = movies_tuple[i]
         for j in range(i+1, tot):
-            id2, (title2, pptitle2, _, neighbors2) = movies_tuple[j]
+            id2, (title2, pptitle2, _, neighbors2, _) = movies_tuple[j]
             if franchise(pptitle1, pptitle2):
                 neighbors1.append(id2)
                 neighbors2.append(id1)
                 edges += 1
-                print(title1, title2)
-    print(edges, len(movies))
-    #for movie in movies.values:
-    #    if movie[1]:
-    #        dfs(movies, movie)
+    f = 1
+    for movie in movies.values():
+        if movie[2]:
+            size = dfs(movies, movie, f)
+            if size > 1:
+                f += 1
+            else:
+                movie[4] = 0
 
 if __name__ == '__main__':
     main()
